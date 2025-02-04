e9834274c4ad314aba8a760af0a41fba8726c29c
nik_git <nikhilbavane24@gmail.com>
2025-02-04 11:41:50 +0530

    Push on 04/02

---
+++ String_Palindrom.py
@@ -0,0 +1,16 @@
+
+
+a="madam1"
+
+b=""
+
+for i in a:
+    b=i+b
+
+print(b)
+
+if a==b:
+    print("String is palindrome")
+
+else:
+    print("String is not palindrome")
---
+++ String_Reverse.py
@@ -0,0 +1,11 @@
+
+
+
+a="Python"
+
+#print(a[::-1])
+b=""
+
+for i in a:
+    b=i+b
+print(b)
