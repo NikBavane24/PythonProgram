57f9614fb12ccdd08e3836f2bc3bdbf36e790731
nik_git <nikhilbavane24@gmail.com>
2025-02-26 14:33:03 +0530

    Commit on 26/02

--- String2.py
+++ String2.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 a="Cricket is the famous game. Cricket help me to be physical fit and famous"
 
@@ -6,3 +7,18 @@ pattern=r'\w*e\b'
 
 word=re.findall(pattern,a)
 print(word)
+
+fr=Counter(a)
+print(fr)
+
+Char_m=fr.get("m")
+print(Char_m)
+
+Min=min(fr,key=fr.get)
+print(Min)
+b=0
+for i in a:
+    if i.isspace():
+        b+=1
+
+print(b)
